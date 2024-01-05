89e0bfbe124024fd98de72899dd732b479bb267d
MICHAELKITH <michaelkithinji27@gmail.com>
2024-01-05 23:56:41 +0300

    Add Rspec

--- spec/models/search_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Search, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:description) }
-    it { should validate_length_of(:description) }
-  end
-end
-
